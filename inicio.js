61df0231137ebeaf8fcf1db4cbb7512a0db29247
juaniike <ignaciojuan975@gmail.com>
2025-03-06 09:28:46 -0300

    Inicio-V1

---
+++ inicio.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mobile-menu").style.width = "100%"; // Muestra el menú
+}
+
+function closeNav() {
+    document.getElementById("mobile-menu").style.width = "0"; // Oculta el menú
+}
