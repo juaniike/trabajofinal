8dba223e9f3679d244ecf75606e99eebc969fea6
juaniike <ignaciojuan975@gmail.com>
2025-03-10 16:02:24 -0300

    Trabajo-V6

--- masas.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("mobile-menu").style.width = "100%"; // Muestra el menú
-}
-
-function closeNav() {
-    document.getElementById("mobile-menu").style.width = "0"; // Oculta el menú
-}
