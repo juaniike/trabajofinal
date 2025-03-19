a132640456a30209d04e7401176fdc3750c575c0
juaniike <ignaciojuan975@gmail.com>
2025-03-19 14:35:03 -0300

    Final-comentado

--- inicio.js
+++ inicio.js
@@ -1,7 +1,13 @@
+/*Función para abrir el menú de navegación móvil*/
 function openNav() {
+    /*Obtiene el elemento con el ID "mobile-menu" y establece su ancho al 100%
+    Esto hace que el menú ocupe toda la pantalla*/
     document.getElementById("mobile-menu").style.width = "100%"; // Muestra el menú
 }
 
+/*Función para cerrar el menú de navegación móvil*/
 function closeNav() {
+    /*Obtiene el elemento con el ID "mobile-menu" y reduce su ancho a 0
+    Esto oculta el menú de la vista*/
     document.getElementById("mobile-menu").style.width = "0"; // Oculta el menú
 }
